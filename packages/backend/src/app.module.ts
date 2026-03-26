0c042b6626d991a722895b0cc59f91a6bbf0f393
Martin Alexanders Lopez Mamani <lopezalexanders@gmail.com>
2026-03-26 08:21:22 -0400

    Fix: Actualizacion de Programas Trabajo final 1.7.3

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -25,11 +25,17 @@ import { ApiModule } from './apps/api/api.module';
       useFactory: () => ({
         type: 'postgres',
         url: process.env.DATABASE_URL,
+
+        ...(!process.env.DATABASE_URL && {
         host: process.env.TYPEORM_HOST ?? 'localhost',
         port: parseInt(process.env.TYPEORM_PORT ?? '5432', 10),
         username: process.env.TYPEORM_USERNAME ?? 'postgres',
         password: process.env.TYPEORM_PASSWORD ?? 'postgres',
         database: process.env.TYPEORM_DATABASE ?? 'academic',
+        }),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        migrationsTableName: 'migrations',
         autoLoadEntities: true,
         synchronize: false,
         ssl: true,
